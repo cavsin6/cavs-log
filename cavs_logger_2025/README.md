10a437b0b8c79f8b7e67405cefc8f28207c75c55
cavsin6 <bro7695@icloud.com>
2025-11-06 04:43:29 -0500

    Add files via upload

---
+++ cavs_logger_2025/README.md
@@ -0,0 +1,2 @@
+# Cavaliers 2025 Logger
+Log and track Cleveland Cavaliers games.
---
+++ cavs_logger_2025/streamlit_app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("Cavaliers 2025 Logger")
+st.info("This is a placeholder app. Upload full code to enable full functionality.")
